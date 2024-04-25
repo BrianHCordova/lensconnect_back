6ae0e1666438979ef64f50de47a6ae763eff0afb
Eric Lee <eric.hyunil.lee@gmail.com>
2024-04-25 12:00:13 -0700

    Set up the routes using jwt to use tokens to interact with front end

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const upload = multer({storage: storage})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 
