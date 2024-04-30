38b9497bdbc2065f0a845f3f13d61b18d17b9b95
Eric Lee <eric.hyunil.lee@gmail.com>
2024-04-29 23:14:07 -0700

    Updated some routes for image and user with rating

--- routes/api/searchRoutes.js
+++ routes/api/searchRoutes.js
@@ -17,5 +17,23 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/featured", async (req, res) => {
+    try {
+        const data = await User.findAll({
+            include: [Review, ServeLocation, Specialty],
+            where: {
+                isPhotographer: true 
+            },
+            order: [
+                ['averageRating', 'DESC'],
+            ],
+        });
+        res.json(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: "error occurred", err });
+    }
+});
+
 
 module.exports = router;
