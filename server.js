ab94c2d67d14415a187c1a60c6461a5b3b3a7a49
anduhrooo <yang.andrew7@gmail.com>
2024-04-30 14:38:25 -0700

    updated chatroom routes, etc

--- server.js
+++ server.js
@@ -44,8 +44,9 @@ const users = {};
 //we can move these to a separate file later
 socketIO.on('connection', (socket) => {
   socket.on('join', (name) => {
-    console.log(name);
+    // console.log(name);
     users[socket.id] = name;
+    // console.log(users);
     console.log(`👥: ${name} just joined!`)
     socket.emit('user-connected', name)
     });
@@ -56,32 +57,34 @@ socketIO.on('connection', (socket) => {
     if (roomValue.room === ''){
       console.log(`📩: ${JSON.stringify(data.user)} says ${JSON.stringify(data.message)} at ${JSON.stringify(data.date)}`);
       
-      // // make a post request here to save the message to the database
-      // fetch('http://localhost:3000/api/chat', {
-      // method: 'POST',
-      // headers: {
-      //   'Content-Type': 'application/json',
-      // },
-      // body: JSON.stringify({
-      //   user_id: data.ID,
-      //   username: data.user,
-      //   message: data.message,
-      // }),
-      // });
+      // make a post request here to save the message to the database
+      fetch('http://localhost:3000/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: data.ID,
+        username: data.user,
+        message: data.message,
+      }),
+      });
+
       socketIO.emit('messageResponse', data);
     } else {
-      console.log(`📩: ${JSON.stringify(data.ID)}: ${JSON.stringify(data.user)} says ${JSON.stringify(data.message)} at ${JSON.stringify(data.date)} in room ${roomValue.room}`);
+      console.log(`📩: ${JSON.stringify(data.user)} says ${JSON.stringify(data.message)} at ${JSON.stringify(data.date)} in room ${roomValue.room}`);
       socket.to(roomValue.room).emit('messageResponse', data);
     }
   });
-  socket.on('joinRoom', (room) => {
-    console.log(`🚪: ${socket.id} joined room ${room.room}`);
-    socket.join(room.room);
-  });
 
-  socket.on('disconnect', () => {
-    console.log(`👋: ${socket.id} user just disconnected!`);
-  });
+    socket.on('joinRoom', (room) => {
+      console.log(`🚪: ${socket.id} joined room ${room.room}`);
+      socket.join(room.room);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`👋: ${socket.id} user just disconnected!`);
+    });
 })
 
 // sync sequelize models to the database, then turn on the server
